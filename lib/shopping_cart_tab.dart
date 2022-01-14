e27ba66c6bfd045ced28bedb017da9cc499d11e9
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-14 18:44:33 -0300

    debug and add a new ShoppingCartItem class

--- lib/shopping_cart_tab.dart
+++ lib/shopping_cart_tab.dart
@@ -145,7 +145,7 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
       AppStateModel model) {
     return SliverChildBuilderDelegate(
           (context, index) {
-            final productIndex = index - 4;
+        final productIndex = index - 4;
         switch (index) {
           case 0:
             return Padding(
@@ -209,6 +209,10 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
               );
             }
         }
+        return null;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -230,5 +234,86 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
         );
       },
     );
+  }
+}
+
+class ShoppingCartItem extends StatelessWidget {
+  const ShoppingCartItem({
+    required this.index,
+    required this.product,
+    required this.lastItem,
+    required this.quantity,
+    required this.formatter,
+    Key? key,
+  }) : super(key: key);
+
+  final Product product;
+  final int index;
+  final bool lastItem;
+  final int quantity;
+  final NumberFormat formatter;
+
+  @override
+  Widget build(BuildContext context) {
+    final row = SafeArea(
+      top: false,
+      bottom: false,
+      child: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          top: 8,
+          bottom: 8,
+          right: 8,
+        ),
+        child: Row(
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(4),
+              child: Image.asset(
+                product.assetName,
+                package: product.assetPackage,
+                fit: BoxFit.cover,
+                width: 40,
+                height: 40,
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          product.name,
+                          style: Styles.productRowItemName,
+                        ),
+                        Text(
+                          formatter.format(quantity * product.price),
+                          style: Styles.productRowItemName,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      '${quantity > 1 ? '$quantity x ' : ''}'
+                          '${formatter.format(product.price)}',
+                      style: Styles.productRowItemPrice,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return row;
   }
 }
