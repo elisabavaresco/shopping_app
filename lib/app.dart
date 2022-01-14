59617ce7575a27e75ebe6d054436ddda1d979c6a
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-14 15:41:47 -0300

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class CupertinoStoreApp extends StatelessWidget {
+  const CupertinoStoreApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // This app is designed only to work vertically, so we limit
+    // orientations to portrait up and down.
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+    return const CupertinoApp(
+      theme: CupertinoThemeData(brightness: Brightness.light),
+      home: CupertinoStoreHomePage(),
+    );
+  }
+}
