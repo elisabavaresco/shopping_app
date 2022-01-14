230cd9713586906db3c47063edd9d604177922da
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-14 17:45:03 -0300

    update the imports and the build() method in _SearchTabState

--- lib/search_tab.dart
+++ lib/search_tab.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import 'model/app_state_model.dart';
+import 'product_row_item.dart';
+import 'search_bar.dart';
+import 'styles.dart';
 
 class SearchTab extends StatefulWidget {
   const SearchTab({Key? key}) : super(key: key);
@@ -10,14 +16,32 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      slivers: <Widget>[
-        CupertinoSliverNavigationBar(
-          largeTitle: Text('Search'),
+    final model = Provider.of<AppStateModel>(context);
+    final results = model.search(_terms);
+
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        color: Styles.scaffoldBackground,
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            _buildSearchBox(),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) => ProductRowItem(
+                  product: results[index],
+                  lastItem: index == results.length - 1,
+                ),
+                itemCount: results.length,
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
