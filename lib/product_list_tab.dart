414cf7a07bd48d0aea09ad5a8dd359ab756f840b
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-14 16:39:26 -0300

    in lib/product_list_tab.dart, import the product_row_item.dart file

--- lib/product_list_tab.dart
+++ lib/product_list_tab.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 
 import 'model/app_state_model.dart';
+import 'product_row_item.dart';
 
 class ProductListTab extends StatelessWidget {
   const ProductListTab({Key? key}) : super(key: key);
