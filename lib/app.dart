20acc3d570189b3e58be22154ebd877e18c2ab86
Elisa Bavaresco <bavaresco.elisa@gmail.com>
2022-01-14 15:57:44 -0300

    at CupertinoStoreHomePage class, swap out the CupertinoPageScaffold for a CupertinoTabScaffold

--- lib/app.dart
+++ lib/app.dart
@@ -23,11 +23,50 @@ class CupertinoStoreHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Store'),
+    return CupertinoTabScaffold(
+      tabBar: CupertinoTabBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'Products',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.shopping_cart),
+            label: 'Cart',
+          ),
+        ],
       ),
-      child: SizedBox(),
+      tabBuilder: (context, index) {
+        late final CupertinoTabView returnValue;
+        switch (index) {
+          case 0:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ProductListTab(),
+              );
+            });
+            break;
+          case 1:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: SearchTab(),
+              );
+            });
+            break;
+          case 2:
+            returnValue = CupertinoTabView(builder: (context) {
+              return const CupertinoPageScaffold(
+                child: ShoppingCartTab(),
+              );
+            });
+            break;
+        }
+        return returnValue;
+      },
     );
   }
 }
